d697de4ee29bbb615cdd4052db8fdb7c7e76ec07
Risingcoderstar <94625582+Risingcoderstar@users.noreply.github.com>
2025-03-21 16:23:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+let player="x";
+let board=new Array(9).fill(null);
+let inp=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8]]
+document.querySelectorAll(".grid").forEach(cell=>{
+cell.addEventListener("click",function(){
+if(board[parseInt(this.id-1)]==null)
+{
+this.innerText=player;
+board[parseInt(this.id)-1]=player;
+player=((player==="o")?"x":"o");
+if(f(player,board)==true)
+{
+document.getElementById("last").innerHTML=`${player} won this match`
+return;
+}
+}
+})
+}
+)
+function f(player,board){
+return inp.some((inde)=>
+inde.every((indexe)=>
+board[indexe]===player)
+)
+}
+
